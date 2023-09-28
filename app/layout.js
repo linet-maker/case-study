4d0d19f10949c4eb22c3ec30e982404cb3c2752a
Samm <velop.acc@gmail.com>
2023-09-28 17:41:56 +0300

    typescript fix 4

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
-import { Analytics } from "@/vercel/analytics/react";
+// eslint-disable-next-line import/no-unresolved
+import { Analytics } from "/@vercel/analytics/react";
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
