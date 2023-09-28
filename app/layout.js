9c03cfb36a12182af1ffc6e426e0ca1ff0c225fc
Samm <velop.acc@gmail.com>
2023-09-28 17:33:25 +0300

    typescript fix number 2

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import { Analytics } from "vercel/analytics/react";
+import { Analytics } from "@/vercel/analytics/react";
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
