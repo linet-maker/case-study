2bdec6d01cb4a2f1d108313397fd1dfb072ce70a
Samm <velop.acc@gmail.com>
2023-09-29 09:47:50 +0300

    Fully purged analytics files

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,10 @@
-import { Analytics } from '../analytics'
+
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body>
 				{children}
-				 <Analytics />
+
 			</body>
 		</html>
 	);
